916fef65e27a86a7f520d2ab58e259b840b43cc0
Glenn Eggleton <geggleto@gmail.com>
2016-11-02 14:52:56 -0400

    First Push

---
+++ src/CreateUser.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Glenn
+ * Date: 2016-11-02
+ * Time: 2:15 PM
+ */
+
+namespace G\Services\User;
+
+
+use G\Core\Http\EndpointInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class CreateUser implements EndpointInterface
+{
+
+    public function __invoke(Request $request, Response $response, array $args)
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
